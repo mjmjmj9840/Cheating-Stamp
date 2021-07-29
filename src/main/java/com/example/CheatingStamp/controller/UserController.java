12952b2468a68f4945b8d73820e296e170550276
kmj <mjmjmj9840@gmail.com>
2021-07-30 07:24:53 +0900

    user에 timestamped 상속 및 lombok 적용

--- src/main/java/com/example/CheatingStamp/controller/UserController.java
+++ src/main/java/com/example/CheatingStamp/controller/UserController.java
@@ -2,21 +2,18 @@ package com.example.CheatingStamp.controller;
 
 import com.example.CheatingStamp.dto.SignupRequestDto;
 import com.example.CheatingStamp.service.UserService;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
+@RequiredArgsConstructor
 @Controller
 public class UserController {
 
     private final UserService userService;
-
-    @Autowired
-    public UserController(UserService userService) {
-        this.userService = userService;
-    }
 
     // 로그인 페이지
     @GetMapping("/user/login")
