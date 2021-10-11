8e22ade809236d5e633b59c451038e8727f5771f
u-nij <u.nij0509@gmail.com>
2021-10-12 02:19:04 +0900

    비디오 stream 촬영, 안내 사항 추가

--- src/main/resources/static/assets/diff-cam-scratchpad/site.js
+++ src/main/resources/static/assets/diff-cam-scratchpad/site.js
@@ -1,6 +1,7 @@
 var video = document.getElementById('video');
 var canvas = document.getElementById('motion');
 var score = document.getElementById('score');
+var notice = document.getElementById('notice');
 
 function initSuccess() {
 	DiffCamEngine.start();
@@ -12,6 +13,13 @@ function initError() {
 
 function capture(payload) {
 	score.textContent = payload.score;
+
+	if (payload.score >= 8000) {
+        notice.textContent = "수험자의 움직임이 경계 범위를 넘었습니다. 주의하세요.";
+	}
+	else {
+	    notice.textContent = "";
+	}
 }
 
 DiffCamEngine.init({
